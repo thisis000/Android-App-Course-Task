a9947082a2792e3c5b74fc7e551e545923175ebb
Arif <107621331+thisis000@users.noreply.github.com>
2026-07-21 23:07:47 +0600

    Create Student array and display information
    Initialize an array of Student objects and print their details.

--- Tasks/T2/Main.java
+++ Tasks/T2/Main.java
@@ -1,1 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+        
+        Student[] s = new Student[5];
 
+        for (int i = 0; i < 5; i++) {
+            s[i] = new Student();
+            s[i].setName("Student-" + i);
+            s[i].setId(201 + i);
+            s[i].setCg(2.5 + (i * 0.1));
+            s[i].setDpt("CSE");
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(s[i].getName() + " " + s[i].getId() + " " + s[i].getCg() + " " + s[i].getDpt());
+        }
+        
+    }
+}
