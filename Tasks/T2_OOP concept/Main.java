8f761c109c3d09a7ec63b0bdb3aa578a1d1dd1d7
Arif <107621331+thisis000@users.noreply.github.com>
2026-07-23 19:10:09 +0600

    Refactor Main.java for university data generation

--- Tasks/T2_OOP concept/Main.java
+++ Tasks/T2_OOP concept/Main.java
@@ -3,30 +3,36 @@ class Main {
     
     public static void main(String[] args) {
 
-        int x = 40;
+        int x = 10;
         University[] uni = new University[x];
 
         for (int i = 0; i < x; i++) {
             uni[i] = new University();
-            uni[i].setUni("DIU");
-            uni[i].setName("Name - " + i);
+            uni[i].setUni(getRandomUni(i));
+            uni[i].setName("Name-" + (i + 1));
             uni[i].setId(456 + i);
             uni[i].setDpt(getRandomDpt(i));
             uni[i].setCg(getGenCG(i));
         }
 
         for (int i = 0; i < x; i++) {
-            Log.d("Output", uni[i].getUni() + " " + uni[i].getName() + " " + uni[i].getId() + " " + uni[i].getDpt() + " " + uni[i].getCg());
+            System.out.println(uni[i].getUni() + " " + uni[i].getName() + " " + uni[i].getId() + " " + uni[i].getDpt() + " " + uni[i].getCg());
         }
     }
+    
+    public static String getRandomUni(int i) {
+        if (i % 3 == 0) return "DIU";
+        else if (i % 4 == 0) return "CUET";
+        else return "AIUB";
+    }
 
-    String getRandomDpt(int i) {
+    public static String getRandomDpt(int i) {
         if (i % 2 == 0) return "CSE";
         else if (i % 5 == 0) return "EEE";
         else return "BBA";
     }
 
-    double getGenCG(int i) {
+    public static double getGenCG(int i) {
         tmpCG += (i * 0.5);
         if (tmpCG < 4) {
             return tmpCG;
