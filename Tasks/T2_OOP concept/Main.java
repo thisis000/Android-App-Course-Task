01a838b00313e056b914bea1ce1b009d401cd12b
Arif <107621331+thisis000@users.noreply.github.com>
2026-07-23 18:58:33 +0600

    Update Main.java

--- Tasks/T2_OOP concept/Main.java
+++ Tasks/T2_OOP concept/Main.java
@@ -1,19 +1,38 @@
 class Main {
+    public static double tmpCG = 2.5;
+    
     public static void main(String[] args) {
-        
-        Student[] s = new Student[5];
 
-        for (int i = 0; i < 5; i++) {
-            s[i] = new Student();
-            s[i].setName("Student-" + i);
-            s[i].setId(201 + i);
-            s[i].setCg(2.5 + (i * 0.1));
-            s[i].setDpt("CSE");
+        int x = 40;
+        University[] uni = new University[x];
+
+        for (int i = 0; i < x; i++) {
+            uni[i] = new University();
+            uni[i].setUni("DIU");
+            uni[i].setName("Name - " + i);
+            uni[i].setId(456 + i);
+            uni[i].setDpt(getRandomDpt(i));
+            uni[i].setCg(getGenCG(i));
         }
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(s[i].getName() + " " + s[i].getId() + " " + s[i].getCg() + " " + s[i].getDpt());
+        for (int i = 0; i < x; i++) {
+            Log.d("Output", uni[i].getUni() + " " + uni[i].getName() + " " + uni[i].getId() + " " + uni[i].getDpt() + " " + uni[i].getCg());
         }
-        
+    }
+
+    String getRandomDpt(int i) {
+        if (i % 2 == 0) return "CSE";
+        else if (i % 5 == 0) return "EEE";
+        else return "BBA";
+    }
+
+    double getGenCG(int i) {
+        tmpCG += (i * 0.5);
+        if (tmpCG < 4) {
+            return tmpCG;
+        } else {
+            tmpCG = 2.5;
+            return 4.0;
+        }
     }
 }
